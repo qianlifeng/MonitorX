7a478eaedbc1e0ba664d6ac98d66fb880e455b9f
qianlifeng <qianlf2008@163.com>
2016-02-24 15:48:36 +0800

    Don't fire forewarning if the time between last notify is too short

--- src/main/java/monitorx/domain/forewarning/ForewarningCheckPoint.java
+++ src/main/java/monitorx/domain/forewarning/ForewarningCheckPoint.java
@@ -12,7 +12,7 @@ public class ForewarningCheckPoint {
 
     boolean hasSendNotify = false;
 
-    public boolean isHasSendNotify() {
+    public boolean hasSendNotify() {
         return hasSendNotify;
     }
 
--- src/main/java/monitorx/domain/forewarning/firerules/BaseFireRule.java
+++ src/main/java/monitorx/domain/forewarning/firerules/BaseFireRule.java
@@ -1,6 +1,7 @@
 package monitorx.domain.forewarning.firerules;
 
 import monitorx.domain.forewarning.FireRuleContext;
+import monitorx.domain.forewarning.ForewarningCheckPoint;
 import monitorx.domain.forewarning.IFireRule;
 import monitorx.domain.forewarning.IFireRuleConfig;
 
@@ -10,14 +11,24 @@ public abstract class BaseFireRule implements IFireRule {
     public boolean isLastNotifyTooShort(FireRuleContext context) {
         if (context.getCheckPoints().size() == 1) return false;
 
-        //todo
+        ForewarningCheckPoint lastNotifyCheckPoint = findLastNotifyCheckPoint(context);
+        if (lastNotifyCheckPoint != null) {
+            ForewarningCheckPoint lastCheckPoint = context.getCheckPoints().get(context.getCheckPoints().size() - 1);
+            long minutes = (lastCheckPoint.getDatetime().getTime() - lastNotifyCheckPoint.getDatetime().getTime()) / (1000 * 60); //minutes
+            if (minutes < 5) {
+                return true;
+            }
+        }
+
         return false;
     }
 
-//    private ForewarningCheckPoint findLastNotifyCheckPoint(FireRuleContext context) {
-//        for (int i = context.getCheckPoints().size() - 1; i >= 0; i--) {
-//            ForewarningCheckPoint point = context.getCheckPoints().get(i);
-//
-//        }
-//    }
+    private ForewarningCheckPoint findLastNotifyCheckPoint(FireRuleContext context) {
+        for (int i = context.getCheckPoints().size() - 1; i >= 0; i--) {
+            ForewarningCheckPoint point = context.getCheckPoints().get(i);
+            if (point.hasSendNotify()) return point;
+        }
+
+        return null;
+    }
 }
